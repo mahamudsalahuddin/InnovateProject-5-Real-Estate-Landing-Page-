9d61dc0e546aebeef16e6821aea3e6536bb59496
Salahuddin Mahmud <76897399+s-mahmud52@users.noreply.github.com>
2022-09-15 10:50:53 +0600

    full project with responsive

---
+++ js/tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          navColor: '#EEEDDE',
+          primary: '#203239',
+          yellow1: '#FF7527',
+          white: '#EEEDDE',
+          cream: '#FFFFFF',
+        },
+        maxWidth: {
+            container: '1166px',
+          },
+          fontFamily: {
+            'poppins':'Poppins',
+            'roboto':'Roboto',
+          },
+          backgroundImage: {
+            navImage: "url('images/navImage.png')",
+          },
+      }
+    }
+  }
